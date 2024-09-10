adee05c01146440d6aee3b8deec8fb44bbd49f36
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-11 00:59:34 +0530

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,86 @@
+		// Declaring and Initializing variables
+		
+/*
+class Lab6
+{
+	public static void main(String[] as)
+	{
+		String str;
+		int a;
+		str="sagar";
+		a=99;
+		System.out.println(str);
+		System.out.println(a);
+	}
+}
+*/
+
+/*
+class Lab7
+{
+	public static void main(String[] as)
+	{
+		int a=99;
+		boolean b=true;
+		String str="sagar";
+		double d=999.99;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(str);
+		System.out.println(d);
+	}
+}
+*/
+
+
+/*
+class Lab8
+{
+	public static void main(String[] as)
+	{
+		int a,byte b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+// ERROR--<identifier> expected
+*/
+
+
+/*
+class Lab9
+{
+	public static void main(String[] as)
+	{
+		int a,b,c;
+		double d;
+		a=b=c=99;d=9.0;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		String str1,str2,str3;
+		str1=str2="sagar"; str3="gorai";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
+
+*/
+
+class Lab10
+{
+	public static void main(String[] as)
+	{
+		int a=120,b=78,c=90;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		String str1="sagar",str2="chandra",str3="gorai";
+		System.out.print(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		
+	}
+}
