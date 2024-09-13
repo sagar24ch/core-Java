4189f3da1586d58a9758be1bca3f093fde5caee7
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-13 22:16:55 +0530

    Add files via upload

---
+++ PracticeTest12.java
@@ -0,0 +1,130 @@
+/*
+class Test21
+{
+	public static void main(String as[])
+	{
+		String email;
+		System.out.println(email);
+	}
+}
+
+
+class Test22
+{
+	public static void main(String[] as)
+	{
+		int a=99;
+		int b=a;
+		System.out.println(a);
+		System.out.println(b);
+		b=88;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+
+class Test23
+{
+	public static void main(String jlc[])
+	{
+		final int AB=99;
+		System.out.println(ab);
+	}
+}
+
+
+class Test23A
+{
+	public static void main(String jlc[])
+	{
+		int AB=99;
+		System.out.println(ab);
+	}
+}
+
+
+class Test24
+{
+	static char val;
+	public static void main(String as[])
+	{
+		System.out.println(val==0);
+	}
+}
+
+class Test25
+{
+	public static void main(String as[])
+	{
+		final int A;
+		System .out.println('A');
+	}
+}
+
+class Test26
+{
+	public static void main(String as[])
+	{
+		static int a;
+		System.out.println(a);
+	}
+}
+
+class Test27
+{
+	public static void main(String as[])
+	{
+		String str1,str2;
+		String str3=str1=str2="jlc";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
+
+class Test27
+{
+	public static void main(String as[])
+	{
+		int  str1,str2;
+		int  str3=str1=str2=10;
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
+
+
+class Test28
+{
+	public static void main(String as[])
+	{
+		int b,c;
+		int a=b=c=99;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
+
+class Test29
+{
+	public static void main(String as[])
+	{
+	int a,b;
+	int c=10,a=20,b=30;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	}
+}
+*/
+class Test30
+{
+	public static void main(String as)
+	{
+		byte b=128;
+		System.out.println(b);
+	}
+}
