dfa5c8ab7da59ec63716082eaa60a5b4e328e873
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-13 18:18:25 +0530

    Add files via upload

---
+++ PracticeTest11.java
@@ -0,0 +1,177 @@
+/*
+
+class Test1{
+	public static void main(String[] as)
+	{
+		int enum=9;
+		System.out.println(enum);
+	}
+}
+
+
+class Test2{
+	public static void main(String[] as)
+	{
+		char charValue='A';
+		System.out.println(charValue);
+	}
+}
+
+
+class Test3{
+	public static void main(String[] as)
+	{
+		byte b=128;
+		System.out.println(b);
+	}
+}
+
+class Test4{
+	public static void main(String[] as)
+	{
+		int a,b,c;
+		a=b=c=999;
+		System.out.println(c);
+	}
+}
+
+class Test5{
+	public static void main(String[] as)
+	{
+		boolean valid=True;
+		System.out.println(valid);
+	}
+	
+}
+
+class Test6{
+	public static void main(String[] as)
+	{
+		int _123=99;
+		System.out.println(_123);
+	}
+	
+}
+
+class Test7{
+	public static void main(String[] as)
+	{
+		String ins="null";
+		System.out.println("String");
+	}
+}
+
+class Test8{
+	public static void main(String[] as)
+	{
+		int $20=1200;
+		System.out.println($20);
+	}
+	
+}
+
+
+class Test9{
+	public static void main(String[] as)
+	{
+		byte $12_34=99;
+		System.out.println($12_34);
+	}
+}
+
+
+class Test10{
+	public static void main(String[] as)
+	{
+		char CHAR='A';
+		System.out.println(CHAR);
+	}
+}
+
+class Test11{
+	public static void main(String[] as)
+	{
+		String String="null";
+		System.out.println(String);
+	}
+}
+*/
+
+/*class Test12{
+	public static void main(String[] as)
+	{
+		char ch;
+		System.out.println(ch == 0);
+	}
+}
+
+
+class Test13{
+	public static void main(String[] as)
+	{
+		int a=10,b=a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+
+class Test14{
+	public static void main(String[] as)
+	{
+		int a=a=10;
+		System.out.println(a);
+	}
+}
+
+class Test15{
+	public static void main(String[] as)
+	{
+		System.out.println($20);
+		
+	}
+}
+
+class Test16{
+	public static void main(String[] as)
+	{
+		int _123,_234;
+		int val=_123=_234=90;
+		System.out.println(val);
+	}
+}
+
+class Test17{
+	public static void main(String[] as)
+	{
+		int a=b,b=10;
+		System.out.println(a);
+	}
+}
+
+class Test18{
+	public static void main(String[] as)
+	{
+		final int ab;
+		ab=12;
+		System.out.println(ab+10);
+		System.out.println(ab);
+	}
+}
+
+class Test19{
+	
+		static String name;
+		public static void main(String[] as)
+		{
+			System.out.println(name);
+	}
+}
+*/
+class Test20{
+	String email;
+	public static void main(String as[])
+	{
+		System.out.println(email);
+	}
+}
