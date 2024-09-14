31ed40f7e040ed9113da669d1bd5e1b9052b218b
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-15 02:08:31 +0530

    Add files via upload

---
+++ PracticeTest2.java
@@ -0,0 +1,120 @@
+/*
+public class Mobile{
+	public static void main(String as[])
+	{
+		double price;
+		//String model;
+		System.out.println(price);
+	}
+}
+
+class Test2
+{
+	public static void main(String as[])
+	{
+		boolean b=0;
+		System.out.println('0');
+	}
+}
+
+class Test3{
+	public static void main(String as[])
+	{
+		boolean faLse=true;
+		boolean b=faLse;
+		System.out.println(b);
+	}
+}
+
+
+class Test4{
+	public static void main(String as[])
+	{
+		char ch='/';
+		System.out.println(ch);
+	}
+}
+
+class Test5
+{
+	public static void main(String as[])
+	{
+		char ch='\';
+		System.out.println(ch);
+	}
+}
+
+
+class Test6
+{
+	public static void main(String as[])
+	{
+		char ch='A';
+		System.out.println(ch+1);
+	}
+}
+
+class Test7
+{
+	public static void main(String as[])
+	{
+		char ch=65535;
+		System.out.println(ch+1);
+	}
+}
+
+class Test8
+{
+	public static void main(String as[])
+	{
+		char ch=0;
+		System.out.println(ch-1);
+	}
+}
+class Test9
+{
+	static String name;
+	public static void main(String as[])
+	{
+		System.out.println(name);
+		System.out.println(name.length());
+	}
+}
+class Test10
+{
+	static String name="null";
+	public static void main(String as[])
+	{
+		System.out.println(name);
+		System.out.println(name.length());
+		
+	}
+}
+
+
+class Test11
+{
+	public static void main(String as[])
+	{
+		String name="Unicode of A is \u0041";
+		System.out.println(name);
+	}
+}
+
+class Test12
+{
+	public static void main(String as[])
+	{
+		String data="value is \61";
+		System.out.println(data);
+	}
+}
+*/
+class Test13
+{
+	public static void main(String as[])
+	{
+		String dir="d:/java/Hello.java";
+		System.out.println(dir);
+	}
+}
