968f7256546b92c872dc8b517650f8fa9d456c35
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-16 22:47:21 +0530

    Add files via upload

---
+++ PracticeTest3.java
@@ -0,0 +1,155 @@
+/*
+class Test1
+{
+	public static void main(String[] args)
+	{
+		float f1=11;
+		float f2=11.f;
+		f2=f1+f2;
+		System.out.println(f2);
+	}
+}
+
+class Test2
+{
+	public static void main(String[] args)
+	{
+		byte b=016;
+		short s=0Xaaa;
+		//char c='1';
+		//int a=b+s+c;
+		System.out.println(s);
+	}
+}
+
+class Test3
+{
+	public static void main(String args[])
+	{
+		float f=12e-1f;
+		final long l=12L;
+		f=f+l;
+		System.out.println(f);
+	}
+}
+
+
+class Hello
+{
+	public static void main(String args[])
+	{
+		int Hello=13;
+		int main=34;
+		System.out.println(Hello+main);
+	}
+}
+
+
+class Test5
+{
+	public static void main(String args[])
+	{
+		int a=7;
+		System.out.println(a);
+	}
+}
+
+class Test6
+{
+	public static void main(String as[])
+	{
+		int a='7';
+		System.out.println(a);
+	}
+}
+
+class Test7
+{
+	public static void main(String as[])
+	{
+		int ab=A;
+		System.out.println(ab);
+	}
+}
+
+class Test8
+{
+	public static void main(String as[])
+	{
+		int ab='A';
+		System.out.println(ab);
+	}
+}
+
+class Test9
+{
+	public static void main(String as[])
+	{
+		int ab=\u0037;
+		System.out.println(ab);
+	}
+}
+
+class Test10
+{
+	public static void main(String as[])
+	{
+	int ab='\u0037';
+	System.out.println(ab);
+	}
+}
+
+class Test11
+{
+	public static void main(String as[])
+	{
+		double d=061;
+		System.out.println(d);
+	}
+}
+
+class Test12
+{
+	public static void main(String as[])
+	{
+		double d=061.0;
+		System.out.println(d);
+	}
+}
+
+class Test13
+{
+	public static void main(String as[])
+	{
+		double d=0XE.P0;
+		System.out.println(d);
+	}
+}
+
+class Test14
+{
+	public static void main(String as[])
+	{
+		double d=077;
+		System.out.println(d);
+	}
+}
+
+class Test15
+{
+	public static void main(String as[])
+	{
+		double d=077.0;
+		System.out.println(d);
+	}
+}
+*/
+
+class Test16
+{
+	public static void main(String as[])
+	{
+		double d=0XA9;
+		System.out.println(d);
+	}
+}
