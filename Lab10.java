892657b64d8b35ee8250954567f1f6c604e4f756
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-17 23:56:53 +0530

    Add files via upload

---
+++ Lab10.java
@@ -0,0 +1,63 @@
+
+/*
+class Lab65
+{
+	public static void main(String as[])
+	{
+		int a=19;
+		int b=+a;
+		int c=-a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+	
+}
+
+class Lab66
+{
+	public static void main(String as[])
+	{
+		byte a=19;
+		byte b=+a;
+		byte c=-a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
+
+class Lab67
+{
+	public static void main(String as[])
+	{
+		char ch1='A';
+		char ch2=+ch1;
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
+class Lab68
+{
+	public static void main(String as[])
+	{
+		char ch1='A';
+		int a=+ch1;
+		System.out.println(ch1);
+		System.out.println(a);
+	}
+}
+*/
+
+class Lab69
+{
+	public static void main(String as[])
+	{
+		double d1=1234.567;
+		double d2=-d1;
+		double d3=+d2;
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d3);
+	}
+}
