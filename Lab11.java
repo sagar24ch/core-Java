dc67e052f9812ceac332761d99b63f19310e0bd9
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-23 16:38:14 +0530

    Add files via upload

---
+++ Lab11.java
@@ -0,0 +1,174 @@
+						// Binary Arithmetic Operator
+/*
+class abi
+{
+	public static void main(String as[])
+	{
+		float f1=11;
+		float f2=11.f;
+		f2=f1+f2;
+		System.out.println(f2);
+	}
+}
+
+class Lab70
+{
+	public static void main(String as[])
+	{
+		byte b1=10;
+		byte b2=20;
+		int b3=b1+b2;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+	}
+}
+
+class Lab72
+{
+	public static void main(String as[])
+	{
+		int a=123;
+		byte b=a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+class Lab73
+{
+	public static void main(String as[])
+	{
+		final int a=123;
+		byte b=a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+class Lab74
+{
+	public static void main(String as[])
+	{
+		final byte b1=90;
+		
+		final byte b2=9;
+		
+		byte b3=b1+b2;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		
+	}
+}
+
+class Lab75
+{
+	public static void main(String as[])
+	{
+		final byte b1=90;
+		final byte b2=38;
+		byte b3=b1+b2;
+		System.out.println(b3);
+	}
+}
+
+class Lab76
+{
+	public static void main(String as[])
+	{
+		final long a=12;
+		byte b1= a;
+		System.out.println(a);
+		System.out.println(b1);
+		
+	}
+}
+
+class Lab77
+{
+	public static void main(String as[])
+	{
+		System.out.println(20/3);
+		System.out.println(20/3.0f);
+		System.out.println(20/3.0);
+	}
+}
+
+class Lab78
+{
+	public static void main(String as[])
+	{
+		System.out.println(20/0.0);
+		System.out.println(20/-0.0);
+		System.out.println(20.0/0);
+		System.out.println(-20.0/0);
+		double d1=10/0.0;
+		System.out.println(d1);
+		
+	}
+}
+
+class Lab79
+{
+	public static void main(String as[])
+	{
+		System.out.println(0/0.0);
+		System.out.println(0/-0.0);
+		System.out.println(0.0/0);
+		System.out.println(-0.0/0.0);
+		System.out.println(0.0/0.0);
+		double d2=0/0.0;
+		System.out.println(d2);
+		}
+}
+
+class Lab80
+{
+	public static void main(String as[])
+	{
+		System.out.println(12/0);
+		System.out.println(0/0);
+	}
+}
+
+class Lab81
+{
+	public static void main(String as[])
+	{
+		int a=90;
+		int b=9;
+		int d=a+b-10;
+		int e=a-b+10;
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
+
+class Lab82
+{
+	public static void main(String as[])
+	{
+		int a=10;
+		int b=20;
+		int c=4;
+		int d=a+b*c;
+		int e=(a+b)*c;
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
+*/
+class Lab83
+{
+	public static void main(String as[])
+	{
+		int a=10;
+		int b=20;
+		int c=4;
+		int d=a-b/c;
+		int e=(a-b)/c;
+		System.out.println(d);
+		System.out.println(e);
+	}
+} 
