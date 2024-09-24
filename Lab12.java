991aacd4f7e3d10a9ec5635ba48d064fabd7bbb9
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-09-24 23:59:42 +0530

    Add files via upload

---
+++ Lab12.java
@@ -0,0 +1,154 @@
+				//for loop
+/*
+class Lab152{
+	public static void main(String as[])
+	{
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+	}
+}
+
+class Lab153
+{
+	public static void main(String as[])
+	{
+		for(int i=1;i<=5;i++)
+			System.out.println(i);
+			System.out.println("hello guys");
+	}
+}
+
+class Lab154
+{
+	public static void main(String as[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(i);
+			System.out.println("hello guys");
+		}
+	}
+}
+
+class Lab155
+{
+	public static void main(String as[])
+	{
+		for (int i=5;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+		System.out.println("hello");
+	}
+}
+
+class Lab156
+{
+	public static void main(String as[])
+	{
+		for(int i=1;i<=10;i++){
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
+
+class Lab157
+{
+	public static void main(String as[])
+	{
+		int n=5;
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println(n+ "*"+i +"=" +(n*i));
+		}
+	}
+}
+
+
+class Lab158
+{
+	public static void main(String as[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			int a=99;
+			a++;
+			System.out.println(i+"\t"+a);
+		}
+	}
+}
+
+class Lab159
+{
+	public static void main(String as[])
+	{
+		int a=99;
+		for(int i=1;i<=5;i++)
+		{
+			a++;
+			System.out.println(i+"\t"+a);
+		}
+	}
+}
+
+class Lab160
+{
+	public static void main(String as[])
+	{
+		for(int i=1;i<=5;i++)
+		
+			System.out.println(i);
+		
+		System.out.println(i);
+	}
+}
+//error i is local variable cannot be used outside for block
+
+class Lab161
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		for(;i<=5;i++)
+		{
+		System.out.println(i);
+		}
+	System.out.println(i);
+	}
+}
+
+class Lab162
+{
+	public static void main(String as[])
+	{
+		 boolean b=true;
+		for(;b;)
+		{
+			System.out.println("inside for");
+			
+		}
+		System.out.println("outside for");
+	}
+}
+
+class Lab163
+{
+	public static void main(String as[])
+	{
+		final boolean b=true;
+		for(;b;)
+		{
+			System.out.println("inside for");
+			
+		}
+		System.out.println("outside for");
+	}
+	
+}
+*/
