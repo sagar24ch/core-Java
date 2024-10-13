f4be9d577e586102d9dd81e567df2ec74bbd2909
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-10-13 18:19:03 +0530

    Add files via upload

---
+++ Lab26.java
@@ -0,0 +1,143 @@
+/*
+class Hello
+{
+	int a=10;
+	static int b=20;
+	
+	void m1()
+	{
+		System.out.println("i am m1()");
+	}
+	static void m2()
+	{
+		System.out.println("i am m2()");
+	}
+	void show()
+	{
+		System.out.println(a);
+		System.out.println(b);
+		m1();
+		m2();
+	}
+}
+class Lab312
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	int a=10;
+	static int b=20;
+	void m1()
+	{
+		System.out.println("i m m1()");
+	}
+	static void m2()
+	{
+		System.out.println("i m m2()");
+	}
+	static void show()
+	{
+		System.out.println(a);
+		System.out.println(b);
+		m1();
+		m2();
+	}
+}
+class Lab313
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	void m1()
+	{
+		System.out.println("i m m1()");
+	}
+}
+class Lab314
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.m1();
+	}
+}
+
+class Hello{
+	void m1()
+	{
+		System.out.println("i am m1()");
+	}
+}
+class Lab315
+{
+	public static void main(String as[])
+	{
+		Hello h=null;
+		h.m1();
+	}
+}
+
+class Hello{
+	void m1(){
+	System.out.println("i am m1()");
+	}
+}
+class Lab316
+{
+	public static void main(String as[])
+	{
+		Hello.m1();
+	}
+}
+
+
+class Hello
+{
+	static void m2(){
+		System.out .println("i am m2");
+	}
+}
+class Lab317
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.m2();
+	}
+}
+
+class Hello{
+	static void m2(){
+		System.out.println("i am m2()");
+	}
+}
+class Lab318{
+	public static void main(String as[])
+	{
+		Hello h=null;
+		h.m2();
+	}
+}
+*/
+class Hello{
+	static void m2(){
+		System.out.println("i am m2()");
+	}
+}
+class Lab319
+{
+	public static void main(String as[])
+	{
+		Hello.m2();
+	}
+}
