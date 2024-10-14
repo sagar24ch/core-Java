3bd2e21e587ca3695e7ae0da6211bbf1a6132e13
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-10-14 16:05:44 +0530

    Add files via upload

---
+++ Lab27.java
@@ -0,0 +1,162 @@
+/*
+class Hello
+{
+	show(){
+		System.out.println("i am show()");
+	}
+}
+class Lab320
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	void show()
+	{
+		System.out.println("i am Show()");
+	}
+}
+class Lab321
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	void show(){
+		System.out.println("i ma show()");
+		return;
+	}
+}
+class Lab322{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello {
+	void show(){
+		System.out.println("i am show()");
+		return 99;
+	}
+}
+class Lab323{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	int show(){
+		System.out.println("i am show()");
+		return 99;
+	}
+}
+class Lab324{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		int x=h.show();
+		System.out.println(x);
+		
+		Hello h1=new Hello();
+		h1.show();
+	}
+}
+
+class Hello{
+	int show(){
+		System.out.println("i am show()");
+	}
+}
+
+class Lab325{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	int show(){
+		System.out.println("i am show()");
+		return;
+	}
+}
+class Lab326{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	int show(){
+		System.out.println("i am show()");
+		return 99L;
+	}
+}
+class Lab327
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	long show(){
+		System.out.println("i am show()");
+		return 99;
+	}
+}
+class Lab328
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	long show(){
+		System.out.println("ok");
+		return 'A';
+	}
+}
+class Lab329{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+*/
+class Hello {
+	boolean show(){
+		System.out.println("okh");
+		return 'A';
+	}
+}
+class Lab330{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
