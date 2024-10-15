3ba7089295640e08de090c50fa3210ca29193db8
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-10-15 10:14:11 +0530

    Add files via upload

---
+++ Lab28.java
@@ -0,0 +1,179 @@
+/*
+class Hello{
+	boolean show(){
+		System.out.println("okh");
+		return true;
+	}
+}
+class Lab331{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	String show(){
+		System.out.println("i m show()");
+		String str="Hello guys!";
+		return str;
+	}
+}
+class Lab332{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		String x=h.show();
+		System.out.println(x);
+	}
+}
+
+class Hello{
+	String show(){
+		System.out.println("ok");
+		return null;
+	}
+}
+class Lab333{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		String x=h.show();
+		System.out.println(x);
+	}
+}
+
+class Hello {
+	int show(){
+		System.out.println("ok");
+		return null;
+	}
+}
+class Lab334
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello{
+	int show(){
+		System.out.println("Hello");
+		return 88;
+		System.out.println("hiiiii");
+		
+	}
+}
+class Lab335{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+class Hello
+{
+	int show(){
+		System.out.println("Hello world");
+		return 88;
+		return 99;
+	}
+}
+class Lab336{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.show();
+	}
+}
+
+
+
+class Hello{
+	boolean isEven(int n){
+	if(n%2==0)
+	{
+		return true;
+	}
+else
+return false;	
+	}
+}
+class Lab337
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		boolean x=h.isEven(9);
+		System.out.println(x);
+	}
+}
+
+class Hello{
+	void sum(int a,int b)
+	{
+		int sum=a+b;
+		System.out.println(sum);
+		
+	}
+}
+class Lab338
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.sum(10,20);
+	}
+}
+
+class Hello{
+	void sum(int a, int b)
+	{
+		int s=a+b;
+		System.out.println(s);
+		
+	}
+}
+class Lab339{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.sum();
+		h.sum(10);
+		
+	}
+}
+
+class Hello{
+	void sum(int a,int b){
+		int s=a+b;
+		System.out.println(s);
+	}
+}
+class Lab340{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.sum(9.8,8.9);
+	}
+}
+*/
+class Hello{
+	void  sum(int a,int b){
+		int s=a+b;
+		System.out.println(s);
+	}
+}
+class Lab341{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		byte b1=10;
+		byte b2=29;
+		h.sum(b1,b2);
+	}
+}
