993b4f785458531fab6412fe23632f9446cb0926
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-11-04 23:32:09 +0530

    Add files via upload

---
+++ PracticeTest41.java
@@ -0,0 +1,22 @@
+/*
+class Test1{
+	public static void main(String as[])
+	{
+		float f1=1.f+0101;
+		System.out.println(f1);
+	}
+}
+
+class Test2{
+	public static void main(String as[])
+	{
+		double d1=1.+0101.0;
+		System.out.println(d1);
+	}
+}
+*/
+class Test3{
+public static void main(String as[]){
+	System.out.println(2147483647+1);
+}
+}
