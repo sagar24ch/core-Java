8e01c96761e718ed2fad51d2a4d66b4996b9cddb
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-11-26 23:27:05 +0530

    Add files via upload

---
+++ Lab22A.java
@@ -0,0 +1,196 @@
+/*
+class Hello{
+	int a=10;
+	static int b=20;
+	void m1()
+	{
+		System.out.println(a);
+		System.out.println(b);
+	}
+	static void m2()
+	{
+		//System.out.println(a);
+		System.out.println(b);
+	}
+}
+class Lab250
+{
+	public static void main(String as[])
+	{
+		Hello.m2();
+	}
+}
+
+class Hello{
+	int a=10;
+	static int b=20;
+	void m1()
+	{
+	System.out.println(a);
+	System.out.println(b);
+}
+	static void m2()
+	{
+		System.out.println(b);
+		
+	}
+}
+class Lab251
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		h.m1();
+		h.m2();
+	}
+}
+
+class Hello {
+	int a;
+}
+
+class Lab252
+{
+	public static void main(String as[])
+	{
+	Hello h=new Hello();
+	System.out.println(h.a);
+	h.a=99;
+	System.out.println(h.a);
+	}
+}
+
+
+class Hello{
+	static int b;
+}
+class Lab253
+{
+	public static void main(String as[])
+	{
+		Hello h=new Hello();
+		System.out.println(h.b);
+		h.b=99;
+		System.out.println(h.b);
+	}
+}
+
+class Hello{
+	int a;
+}	
+class Lab254
+{
+	public static void main(String as[])
+	{
+		Hello h1=new Hello();
+		Hello h2=new Hello();
+		System.out.println(h1.a+"\t"+h2.a);
+		h1.a=99;
+		System.out.println(h1.a+"\t"+h2.a);
+	}
+}
+
+class Hello{
+	static int a;
+}
+class Lab255
+{
+	public static void main(String as[])
+	{
+		Hello h1=new Hello();
+		Hello h2=new Hello();
+		System.out.println(h1.a+"\t"+h2.a);
+		h1.a=99;
+		System.out.println(h1.a+"\t"+h2.a);
+	}
+}
+
+class Hello{
+	int a;
+}	
+class Lab256
+{
+	public static void main(String as[])
+	{
+		new Hello().a=99;
+		System.out.println(new Hello().a);
+	}
+}
+
+class Hello{
+	static int b;
+	
+}
+class Lab257
+{
+	public static void main(String as[])
+	{
+		new Hello().b=99;
+		System.out.println(new Hello().b);
+	}
+}
+
+class Hello{
+	int a;
+}
+class Lab258
+{
+	public static void main(String as[])
+	{
+		Hello h1=null;
+		System.out.println(h1.a);
+	}
+}
+
+class Hello{
+	static int b;
+	
+}
+class Lab259
+{
+	public static void main(String as[])
+	{
+		Hello h1=null;
+		System.out.println(h1.b);
+	}
+}
+
+class Hello{
+	int a;
+}
+class Lab260
+{
+	public static void main(String as[])
+	{
+		System.out.println(Hello.a);
+	}
+}
+
+class Hello{
+	static int b;
+	}
+class Lab261
+{
+	public static void main(String as[])
+	{
+		System.out.println(Hello.b);
+	}
+
+}
+
+class Lab262
+{
+	int a;
+	public static void main(String as[])
+	{
+		System.out.println(a);
+	}
+}*/
+class Lab263
+{
+	static int b;
+	public static void main(String as[])
+	{
+		System.out.println(b);
+	}
+}
