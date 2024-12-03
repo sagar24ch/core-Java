daa054bc136bc55ea703a4fc263ff64601b8390f
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-12-03 23:08:59 +0530

    Add files via upload

---
+++ Lab17A.java
@@ -0,0 +1,96 @@
+/*
+class Lab216
+{
+	public static void main(String as[])
+	{
+		int arr[]={10,20,30,40,50};
+		System.out.println("size="+arr.length);
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		}
+}
+
+
+class Lan217
+{
+	public static void main(String as[])
+	{
+		double d[]={1.2,3.4,5.6,7.8,9.0};
+		System.out.println(d.length);
+		
+		for(int i=0;i<d.length;i++)
+		{
+			System.out.println(d[i]);
+		}			
+	}
+}
+
+class Lab218
+{
+	public static void main(String as[])
+	{
+		double arr[]={1,2,3,4,5,6.6};
+		System.out.println(arr.length);
+	}
+}
+
+class Lab219
+{
+	public static void main(String as[])
+	{
+		int arr[]={};
+		System.out.println(arr.length);
+	}
+}
+
+class Lab220
+{
+	public static void main(String as[])
+	{
+		int arr[]={};
+		System.out.println(arr.length);
+				System.out.println(arr[0]);
+
+	}
+}
+
+class Lan221
+{
+	public static void main(String as[])
+	{
+		int arr[]=new int[]{10,20,30,40,50};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr.length);
+		}
+	}
+}
+
+class Lab222
+{
+	public static void main(String as[])
+	{
+		int arr[]=new int[5]{10,20,30,40,50,60};
+		for (int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
+*/
+class Lab223
+{
+	public static void show(int arr[])
+	{
+		for (int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+	public static void main(String jlc[])
+	{
+		show(new int[]{10,20,30});
+	}
+}
+
