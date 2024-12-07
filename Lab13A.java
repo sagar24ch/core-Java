bdc942b5406e7c02446d19fc49c925f373208bd2
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-12-07 23:28:54 +0530

    Add files via upload

---
+++ Lab13A.java
@@ -0,0 +1,225 @@
+						//looping control statement ...while and do-while
+/*
+class Test12
+{
+	public static void main(String as[])
+	{
+		int num=5;
+		Mycod:
+		for (int i=2;i<num;i++)
+			if(num%i!=0)
+		{
+			continue Mycod;
+			System.out.println(num+"is prime number");
+		}
+		
+	}
+}
+
+class Lab165
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println(1);
+			System.out.println("Hello");
+		}
+		System.out.println("ok");
+	}
+}
+
+class Lab166
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		while (i<=5)
+		{
+			System.out.println(i);
+			System.out.println("Hello");
+			i++;
+		}
+		System.out.println("ok");
+	}
+}
+
+class Lab167
+{
+	public static void main(String as[])
+	{
+		byte i=1;
+		while(i<=5)
+		{
+			System.out.println(i);
+			System.out.println("Hello");
+			i--;
+		}
+		System.out.println("ok");
+	}
+}
+
+class Lab168
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		while(i<=5)
+		{
+		System.out.println(i);
+		System.out.println("hello");
+		i--;
+		}
+		System.out.println("ok");
+	}
+}
+
+class Lab169
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		while(i++<=5)
+		{
+			System.out.println(i);
+		}
+		System.out.println("Outside:"+i);
+	}
+}
+
+class Lab170
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		while(++i<=5)
+		{
+			System.out.println(i);
+		}
+		System.out.println("Outside;"+i);
+	}
+}
+
+
+class Lab171
+{
+	public static void main(String as[])
+	{
+		while()
+		{
+			System.out.println("sagar");
+		}
+	}
+}
+*/
+class Lab172
+{
+	public static void main(String as[])
+	{
+		while(true)
+		{
+			System.out.println("sagar");
+		}
+	}
+}
+/*
+class Lab173
+{
+	public static void main(String as[])
+	{
+		while(true)
+		{
+			System.out.println("sagar");
+		}
+		System.out.println("outside while");
+	}
+}
+
+class Lab174
+{
+	public static void main(String as[])
+	{
+		boolean b=true;
+		while(b)
+		{
+			System.out.println("sagar");
+		}
+		System.out.println("outside-while");
+	}
+}
+
+class Lab175
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		do{
+			System.out.println(i);
+			System.out.println("Hello");
+			i++;
+		}
+		while(i<=5);
+	}
+}
+
+class Lab176
+{
+	public static void main(String as[])
+	{
+		int i=5;
+		do{
+			System.out.println(i);
+			System.out.println("Hello");
+			i--;
+		}while(i>=1);
+		
+	}
+}
+
+class Lab177
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		do{
+			System.out.println(i++);
+		}while(i<=5);
+	}
+}
+
+class Lab178
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		do{
+			System.out.println(++i);
+		}while(i<=5);
+	}
+}
+
+class Lab179
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		do{
+			System.out.println(i++);
+		}while(true);
+	}
+}
+
+class Lab180
+{
+	public static void main(String as[])
+	{
+		int i=1;
+		do{
+			System.out.println(i++);
+		}while(true);
+		System.out.println("outside-do while");
+	}
+}
+//error: unreachable statement
+*/
