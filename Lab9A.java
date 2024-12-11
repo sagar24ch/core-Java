895263bb4c15e1548d6537c1b40a8501e96e8426
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-12-11 18:15:21 +0530

    Add files via upload

---
+++ Lab9A.java
@@ -0,0 +1,141 @@
+/*
+class Lab53
+{
+	public static void main(String as[])
+	{
+		double d1=1234.567;
+		double d2=123.4567;
+		System.out.println(d1);
+		System.out.println(d2);
+	}
+}
+
+class Lab54
+{
+	public static void main(String as[])
+	{
+		float f1=12987.63;
+		System.out.println(f1);
+	}
+}
+class Lab55
+{
+	public static void main(String as[])
+	{
+		float f1=12987.63F;
+		System.out.println(f1);
+	}
+}
+class Lab56
+{
+	public static void main(String as[])
+	{
+		float f1=129.8763e2F;
+		double d1=1298.763e+2;
+		double d2=12987.63e-2;
+		float f2=12.87e0F;
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(d1);
+		System.out.println(d2);
+	}
+}
+
+class Lab57
+{
+	public static void main(String as[])
+	{
+		double d1=0xaP0;
+		System.out.println(d1);
+		double d2=0xap1;
+		System.out.println(d2);
+		double d3=0xaP2;
+		System.out.println(d3);
+		double d4=0XA9.0P0;
+		double d5=0XA9.P0;
+		System.out.println(d4);
+		System.out.println(d5);
+		int a1=0Xa9;
+		System.out.println(a1);
+	}
+}
+
+class Lab58
+{
+	public static void main(String as[])
+	{
+		double d1=0Xa12.12;
+		System.out.println(d1);
+	}
+}
+
+class Lab59
+{
+	
+	static String str1;
+
+	public static void main(String str[])
+	{
+		String str2=null;
+		System.out.println(str1);
+		System.out.println(str2);
+	}
+}
+
+class Lab60
+{
+	public static void main(String as[])
+	{
+		int a=2_45_12_452;
+		System.out.println(a);
+	}
+}
+
+class Lab61
+{
+	public static void main(String as[])
+	{
+		int _12=9898;
+		System.out.println(_12);
+		int a=_12;
+		System.out.println(a);
+	}
+}
+
+class Lab62
+{
+	public static void main(String as[])
+	{
+		int a=_234;
+		System.out.println(a);
+	}
+}
+
+class Lab63
+{
+	public static void main(String as[])
+	{
+		int a=0_77;
+		System.out.println(a);
+		int b=0b1_01_01;
+		System.out.println(b);
+		int c=0X1_A_F;
+		System.out.println(c);
+		double d=1_2_3.4_5_6;
+		System.out.println(d);
+	}
+	
+}
+*/
+class Lab64
+{
+	public static vooid main(String as[])
+	{
+		int a=234_;
+		int b=0b_101;
+		int c=0X_1_A_F;
+		double d1=123_.45;
+		double d2=123._34;
+	}
+}
+	error: illegal underscore
