c409707ceb938715a1be86426019dbbde0e2032a
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-12-12 20:35:45 +0530

    Add files via upload

---
+++ Lab8A.java
@@ -0,0 +1,118 @@
+/*
+class Lab42
+{
+	public static void main(String as[])
+	{
+		byte b=65;
+		System.out.println(b);
+		short s=1234;
+		System.out.println(s);
+		int i=123431;
+		System.out.println(i);	
+	}
+}
+
+class Lab43
+{
+	public static void main(String as[])
+	{
+		byte b=128;
+		short s=32768;
+		int i=2147483648;
+		long l=2147483648;
+		
+	}
+}
+
+class Lab44
+{
+	public static void main(String as[])
+	{
+		long l=2147483648L;
+		System.out.println(l);
+	}
+}
+
+class Lab45
+{
+	public static void main(String as[])
+	{
+		System.out.println(9880979999);
+	}
+}
+
+class Lab46
+{
+	public static void main(String as[])
+	{
+		System.out.println(9880979999L);
+	}
+}
+
+
+class Lab47
+{
+	public static void main(String as[])
+	{
+		int a=099;
+		System.out.println(a);
+	}
+}
+
+class Lab48
+{
+	public static void main(String as[])
+	{
+		int a=1010;
+		int b=0101;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+class Lab49
+{
+	public static void main(String as[])
+	{
+			int a=0xA;
+			int b=0x61;
+			int c=0xface;
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+	}
+}
+
+class Lab50
+{
+	public static void main(String as[])
+	{
+		int a=101;
+		int b=0b101;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+class Lab51
+{
+	public static void main(String as[])
+	{
+		int b=0b102; 
+		System.out.println(b);
+	}
+}
+*/
+class Lab52{
+	public static void main(String as[])
+	{
+		int a=101;
+		int b=0101;
+		int c=0x101;
+		int d=0b101;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
