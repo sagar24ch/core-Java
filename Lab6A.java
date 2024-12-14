08e1a7570662aa9158cf5c361d9e46855946c3c4
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2024-12-14 17:25:32 +0530

    Add files via upload

---
+++ Lab6A.java
@@ -0,0 +1,155 @@
+/*
+
+class Lab30
+{
+	public static void main(String as[])
+	{
+		char ch1=65;
+		char ch=48;
+		char ch2=97;
+		System.out.println(ch1);
+		System.out.println(ch);
+		System.out.println(ch2);
+	}
+}
+
+class Lab31
+{
+	public static void main(String as[])
+	{
+		char ch='1';
+		System.out.println(ch);
+	}
+}
+
+class Lab32
+{
+	public static void main(String as[])
+	{
+		char ch=1;
+		System.out.println(ch);
+	}
+}
+
+class Lab33
+{
+	public static void main(String as[])
+	{
+		char ch1=0;
+		char ch2=65535;
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
+
+class Lab34
+{
+	public static void main(String as[])
+	{
+		char ch1=-1;
+		char ch2=256;
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
+
+class Lab35
+{
+	public static void main(String as[])
+	{
+		char ch1='\u0061';
+		char ch2='\u0062';
+		System.out.println(ch1);
+		System.out.println(ch2);
+		char ch3='\u0041';
+		char ch4='\u0060';
+		System.out.println(ch3);
+		System.out.println(ch4);
+	}
+}
+
+class Lab36
+{
+	public static void main(String as[])
+	{
+		char ch1='A';
+		char ch2=65;
+		System.out.println(ch1);
+		System.out.println(ch2);
+		char ch3='\u0041';
+		char ch4='\101';
+		System.out.println(ch3);
+		System.out.println(ch4);
+	}
+}
+
+class Lab37
+{
+	public static void main(String as[])
+	{
+		char ch1='\101';
+		char ch2='\103';
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
+class Lab38
+{
+	public static void main(String as[])
+	{
+		char ch1='\101';
+		char ch2='\420';
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
+
+class Lab39
+{
+	String str1=null;
+		System.out.println(str1);
+		int x=str1.length();
+		System.out.println(x);
+	//public static void main(String as[])
+	//{
+		
+	//}
+}
+
+
+
+
+
+
+
+
+class Lab40
+{
+	public static void main(String as[])
+	{
+		String str1="";
+		System.out.println(str1);
+		int x=str1.length();
+		System.out.println(x);
+		String str2="SaharIndia";
+		System.out.println(str2);
+		int y=str2.length();
+		System.out.println(y);
+		String str3="Sagar@gmail.com";
+		System.out.println(str3);
+		int z=str3.length();
+		System.out.println(z);
+	}
+}
+*/
+class Lab41
+{
+	public static void main(String as[])
+	{
+		String dir1="D:\new\test\batch";
+		System.out.println(dir1);
+		String dir2="D:\\new\\test\\batch";
+		System.out.println(dir2);
+	}
+}
+
