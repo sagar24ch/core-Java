8569e67949befec86e0cb2d3c1fc3aeb4556e290
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2025-04-25 22:12:20 +0530

    Add files via upload

---
+++ Lab469.java
@@ -0,0 +1,13 @@
+public class Lab469 {
+    public static void main(String as[]){
+        int arr[]=new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=6;
+        System.out.println(arr);
+        System.out.println("length of array="+arr.length);
+        for(int x:arr){
+            System.out.println(x);
+        }
+    }
+}
